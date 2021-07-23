6598b5c647da2b01848c304254bede3d5a3f0f8e
soon.good <playmymind@daum.net>
2021-07-23 22:13:33 +0900

    프로젝트 초기 버전

---
+++ src/test/java/io/study/ehcache/EhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
